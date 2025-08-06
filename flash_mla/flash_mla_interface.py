60079efa63d9b13549a576a0adc09c4ecaad0d1e
Matthew Bonanni <mbonanni001@gmail.com>
2025-08-06 20:32:54 +0000

    FP8 now extends base implementation
    Signed-off-by: Matthew Bonanni <mbonanni001@gmail.com>

--- flash_mla/flash_mla_interface.py
+++ flash_mla/flash_mla_interface.py
@@ -33,6 +33,8 @@ def flash_mla_with_kvcache(
     num_splits: torch.Tensor,
     softmax_scale: Optional[float] = None,
     causal: bool = False,
+    descale_q: Optional[torch.Tensor] = None,
+    descale_k: Optional[torch.Tensor] = None,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
     Arguments:
@@ -45,6 +47,8 @@ def flash_mla_with_kvcache(
         num_splits: (batch_size + 1), torch.int32, returned by get_mla_metadata.
         softmax_scale: float. The scale of QK^T before applying softmax. Default to 1 / sqrt(head_dim).
         causal: bool. Whether to apply causal attention mask.
+        descale_q_: (batch_size), torch.float32. Descaling factors for Q, used for fp8 quantization.
+        descale_k_: (batch_size), torch.float32. Descaling factors for K, used for fp8 quantization.
 
     Returns:
         out: (batch_size, seq_len_q, num_heads_q, head_dim_v).
@@ -62,5 +66,7 @@ def flash_mla_with_kvcache(
         causal,
         tile_scheduler_metadata,
         num_splits,
+        descale_q,
+        descale_k,
     )
     return out, softmax_lse
